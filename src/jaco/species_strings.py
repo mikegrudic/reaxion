d252f7a570419eba6907f6381a280097d9a9b69f
Mike Grudic <mike.grudic@gmail.com>
2025-11-27 17:03:27 -0500

    add file

---
+++ src/jaco/species_strings.py
@@ -0,0 +1,147 @@
+"""
+Routines for parsing and manipulating chemical species' string identifiers. All such behaviours should be defined
+here - i.e. this is where all of jaco's conventions for writing down chemical formulae should be.
+
+# C_6H_12O_63+ ambiguous: 6 O's with 3+ charge or 63 O's with 1+ charge?
+# assume any contiguous number following a _ is the number of the species in the formula, if you want to
+# make sure something gets identified as a charge put a ^
+
+- underscores and spaces are valid for readability and latex compatibility, but are ignored in the spec. All should be
+compatible with UMIST.
+
+is C3+ (C_3)+ or C^{3+}?
+"""
+
+from string import digits
+from .data.atoms import atoms, atomic_weights
+from astropy.constants import k_B, m_p, m_e
+from astropy import units as u
+
+boltzmann_cgs = k_B.to(u.erg / u.K).value
+protonmass_cgs = m_p.to(u.g).value
+electronmass_cgs = m_e.to(u.g).value
+
+
+def strip(species: str) -> str:
+    """Strip all extraneous characters from the species that will be ignored"""
+    stripped = "".join(species.split())  # whitespace
+    stripped = stripped.replace("_", "")
+    return stripped
+
+
+def species_charge(species: str) -> int:
+    """Returns the charge number of a species from its name"""
+    if species[-1] not in ("-", "+"):
+        return 0
+    if "++" in species:
+        return 2
+    elif "--" in species:
+        return -2
+
+    base = base_species(species)
+    suffix = species.split(base)[-1]
+    if suffix == "+":
+        return 1
+    if suffix == "-":
+        return -1
+    elif "+" in suffix:
+        return int(suffix.rstrip("+"))
+    else:
+        return -int(suffix.rstrip("-"))
+
+
+def is_an_ion(species: str) -> bool:
+    return species_charge(species) != 0 and species != "e-"
+
+
+def base_species(species: str) -> str:
+    """Removes the charge suffix from a species"""
+    # FIXME: WILL FAIL FOR GENERAL CHEMICAL FORMULAE
+    if species == "e-":
+        return "e-"
+    base = species.rstrip(digits + "-+")
+    return base
+
+
+def neutralize(species: str) -> str:
+    """If an ion, return the neutralized version, otherwise just return the species"""
+    if species == "e-":
+        raise NotImplementedError("neutralization of e- is undefined")
+    return base_species(species)
+
+
+def charge_suffix(charge: int) -> str:
+    """Returns the suffix for an input charge number"""
+    match charge:
+        case 0:
+            return ""
+        case 1:
+            return "+"
+        case 2:
+            return "++"
+        case -1:
+            return "-"
+        case -2:
+            return "--"
+        case _:
+            if charge < -2:
+                return str(abs(charge)) + "-"
+            else:
+                return str(abs(charge)) + "+"
+
+
+def ionize(species: str) -> str:
+    """Returns the symbol of the species produced by removing an electron from the input species"""
+    charge = species_charge(species)
+    return base_species(species) + charge_suffix(charge + 1)
+
+
+def recombine(species: str) -> str:
+    """Returns the symbol of the species produced by adding an electron to the input species"""
+    charge = species_charge(species)
+    return base_species(species) + charge_suffix(charge - 1)
+
+
+def species_components(species: str) -> dict:
+    """Returns a dict of components (nuclei, electrons) found in a species whose values are the number of that species
+    found in it"""
+
+    if species == "e-":  # special behaviour
+        return {"e-": 1}
+
+    components = {}
+    charge = species_charge(species)
+    formula = base_species(species)  # take off charge suffix
+    formula = "".join(formula.split())  # remove whitespace
+
+    elements = []
+    # scan through looking for elements, adding up the numbers that follow. try 2-letter elements first
+    for length in (2, 1):
+        if formula[:length] in atoms:
+            elements.append(formula[:2])
+            formula = formula[length:]
+
+    return components
+
+
+# def species_mass(species: str) -> float:
+#     """
+#     Returns the mass of a species in g
+
+#     Parameters
+#     ----------
+#     species: string
+#         Identifier for the species
+
+#     Returns
+#     -------
+#     mass: float
+#         mass of species in g
+#     """
+
+#     # proper way to do this: write function that decomposes a general species into nuclei + electron
+#     if species in atomic_weights:  # atom
+#         return protonmass_cgs * atomic_weights[species]
+#     elif neutralize(species) in atomic_weights:  # atomic ion
+#         return protonmass_cgs * atomic_weights[neutralize(species)] - electronmass_cgs * species_charge
+#     elif "H_2" in
